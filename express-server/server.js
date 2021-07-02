ed1f308de686e66e3dca5cb3a1a50d79ef6f7a99
NickAzz <nick.azurin@gmail.com>
2021-07-02 05:07:49 +0000

    refactored server to connect to vagrant databases and env processing

--- express-server/server.js
+++ express-server/server.js
@@ -2,11 +2,19 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+require('dotenv').config();
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+
+//Database connection configuration
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js')
+const db = new Pool(dbParams);
+console.log(dbParams)
+db.connect();
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
